98b1d795c0c1432e466dc75e3543362c9a159019
Aaron Bieber <aaron@bolddaemon.com>
2018-01-14 10:52:30 -0700

    template for env.production, rake helper script

---
+++ roles/mastodon/files/rake_runner.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+case $1 in
+	"db")
+		SAFETY_ASSURED=1 RAILS_ENV=production bundle24 exec rails db:setup && \
+		touch /var/www/_mastodon/db
+		;;
+	"cssjs")
+		ulimit -n $(ulimit -Hn)
+		ulimit -d $(ulimit -Hd)
+		RAILS_ENV=production bundle24 exec rails assets:precompile && \
+		touch /var/www/_mastodon/cssjs
+		;;
+	*)
+		exit 1
+		;;
+esac
